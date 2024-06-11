463a479a388ac782e109576769ee287605afe414
Maciej <mechlinskimaciej@gmail.com>
2024-06-12 01:27:34 +0200

    local ip config

--- server.py
+++ server.py
@@ -87,4 +87,4 @@ async def read_patient(patient_id: int, current_user: schemas.User = Depends(get
     return db_user
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="192.168.1.228", port=8000)
